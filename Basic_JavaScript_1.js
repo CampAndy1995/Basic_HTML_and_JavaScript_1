e53c8c12e8b4b6ae5ebacad345b7afbcc9ba7ab6
CampAndy1995 <144943158+CampAndy1995@users.noreply.github.com>
2023-10-27 09:21:50 -0700

    first commit

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head> 
+		<title> The Big Bad Wolf</title>
+	</head>
+	<body>
+		<script>
+		//javascript alert box
+		alert("Hi I am an alert box"!)
+		</script>
+	</body>
+</html>
